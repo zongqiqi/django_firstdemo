7ae49c29fe0b17966340da0cc597ce7729b87b6e
zongqiqi <544136329@qq.com>
2017-10-14 23:55:51 +0800

    love:login

--- lovestory/urls.py
+++ lovestory/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-	url(r'^$',views.index,name='index'),
+	# url(r'^$',views.index,name='index'),
+	url(r'^$',views.test,name='test'),
+	url(r'^login$',views.login,name='login'),
 
 ]
--- lovestory/views.py
+++ lovestory/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect,Http404
 
 import os
 
-def index(request):
-	images=[]
-	path=r'/static/lovestory/loveimage/'
-	pwd=os.path.dirname(__file__)
-	dest=pwd+path
-	files=os.listdir(dest)
-	for file in files:
-		mijp=os.path.splitext(file)[1]
-		if mijp=='.jpg':
-			file2='lovestory/loveimage/'+file
-			images.append(file2)
-	context={'images':images}
-	return render(request,'lovestory/index.html',context)
+# def index(request):
+# 	images=[]
+# 	path=r'/static/lovestory/loveimage/'
+# 	pwd=os.path.dirname(__file__)
+# 	dest=pwd+path
+# 	files=os.listdir(dest)
+# 	for file in files:
+# 		mijp=os.path.splitext(file)[1]
+# 		if mijp=='.jpg':
+# 			file2='lovestory/loveimage/'+file
+# 			images.append(file2)
+# 	context={'images':images}
+# 	return render(request,'lovestory/index.html',context)
+
+def test(request):
+	return render(request,'lovestory/login.html')
+	# if request.GET['username'] in ['zongqiqi','wangxiao'] and request.GET['password'] in ['xy0522']:
+	# 	return HttpResponseRedirect(reverse('love:index',))
+def login(request):
+	if request.GET['username'] in ['zongqiqi','wangxiao'] and request.GET['password'] in ['xy0522']:
+		# return HttpResponseRedirect(reverse('love:index',))
+		images=[]
+		path=r'/static/lovestory/loveimage/'
+		pwd=os.path.dirname(__file__)
+		dest=pwd+path
+		files=os.listdir(dest)
+		for file in files:
+			mijp=os.path.splitext(file)[1]
+			if mijp=='.jpg':
+				file2='lovestory/loveimage/'+file
+				images.append(file2)
+		context={'images':images}
+		return render(request,'lovestory/index.html',context)
