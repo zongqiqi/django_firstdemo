c19f8f31d1e5f5050189d76f5571b66e32163dcf
zongqiqi <544136329@qq.com>
2017-11-21 15:03:50 +0800

    308

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'reading',
     'mytest',
     'lovestory',
+
+
 ]
 
 MIDDLEWARE = [
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
     url(r'^learning_log/',include('learning_logs.urls',namespace='learning_logs')), 
     url(r'^users/',include('users.urls',namespace='users')),
